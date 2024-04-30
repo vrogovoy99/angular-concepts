1188b3f597e4ec32e0e50af390860c91882a6bcc
Human <vladislav.rogovoy@gmail.com>
2024-04-30 11:47:44 -0400

    Created app.component Component AppComponent and displayed in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,20 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  template: 'Hello! I`m your cusine',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-concepts';
+  title = 'Hello! I`m your cusine...';
+  city = 'New York';
 }
+
+// @Component({
+//   selector: 'app-user', 
+//   template: '<h2>logged in as {{username}}',
+//   standalone: true
+// })
+// export class UserComponent {
+//   username = 'Biggest User';
+// }
